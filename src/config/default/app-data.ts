0b375b9aa4d8ea9e5d01d0b19a38d45e868da896
Reddy <ashwath10110@gmail.com>
2019-03-14 21:23:02 +0530

    Fixed styles

--- src/config/default/app-data.ts
+++ src/config/default/app-data.ts
@@ -106,6 +106,6 @@ export const DEFAULT_APP_DATA = {
   ],
   contact_info: {
     contact_no: '888-888-xxx',
-    copyright: 'Copyright © 2018 AviaCommerce, Inc.'
+    copyright: 'Copyright © 2018 MR Technologies, Inc.'
   }
 };
